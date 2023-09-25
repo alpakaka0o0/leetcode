484d63297b7caebd8eef0ddc6f55bb88d8c5e3f0
alpakaka0o0 <115137708+alpakaka0o0@users.noreply.github.com>
2023-09-25 21:10:53 +0900

    Time: 73 ms (36.17%), Space: 59.5 MB (5.51%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -5,17 +5,16 @@ public:
         int right = height.size() - 1;
         int maxArea = 0;
 
-        while (left < right) {
+        while (left < right){
             int currentArea = min(height[left], height[right]) * (right - left);
             maxArea = max(maxArea, currentArea);
 
-            if (height[left] < height[right]) {
+            if (height[left] < height[right]){
                 left++;
-            } else {
+            }else{
                 right--;
             }
         }
-
         return maxArea;
     }
 };
