068965568d2aab4bfaa007c117463edce40e6c31
alpakaka0o0 <115137708+alpakaka0o0@users.noreply.github.com>
2025-02-12 15:32:46 +0900

    Time: 2 ms (76.55%), Space: 45 MB (25.83%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -4,33 +4,32 @@ class Solution {
         HashSet<Character>[] cols = new HashSet[9];
         HashSet<Character>[] boxes = new HashSet[9];
 
-        for (int i = 0; i<9; i++){
+        for (int i =0; i < 9; i++){
             rows[i] = new HashSet<>();
             cols[i] = new HashSet<>();
             boxes[i] = new HashSet<>();
         }
 
         for (int r = 0; r < 9; r++){
-            for (int c= 0; c < 9; c++){
-                if (board[r][c] == '.'){
+            for (int c = 0; c < 9; c++){
+                if (board[r][c] == '.') {
                     continue;
                 }
 
-                char value = board[r][c];
-                int boxIndex = (r/3) * 3 + (c/3);
+                char num = board[r][c];
 
-                if (rows[r].contains(value) || cols[c].contains(value) || boxes[boxIndex].contains(value)){
+                int boxIndex = (r / 3) * 3  + (c/3);
+
+                if (boxes[boxIndex].contains(num) || rows[r].contains(num) || cols[c].contains(num)){
                     return false;
                 }
 
-                rows[r].add(value);
-                cols[c].add(value);
-                boxes[boxIndex].add(value);
+                rows[r].add(num);
+                cols[c].add(num);
+                boxes[boxIndex].add(num);
             }
         }
         return true;
-
-        
 
     }
 }
