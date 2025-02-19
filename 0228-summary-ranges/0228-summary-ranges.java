74e64c7ddb5c70a832a69c0a497c0a5352bdfd39
alpakaka0o0 <115137708+alpakaka0o0@users.noreply.github.com>
2025-02-19 13:40:29 +0900

    Time: 6 ms (29.03%), Space: 41.8 MB (68.59%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,25 +1,19 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        List<String> answer = new ArrayList<>();
-        if (nums.length == 0) return answer;
+        List<String> result = new ArrayList<>();
+        if (nums.length == 0) return result;
 
-
-        int prev = nums[0];
-        for (int i = 0; i < nums.length-1; i++){
-            if(nums[i+1] - nums[i] != 1){
-                if (nums[i] == prev){
-                    answer.add(Integer.toString(nums[i]));
-                }else{
-                    answer.add(prev + "->"+nums[i]);
+        int start = nums[0];
+        for (int i = 1; i <= nums.length; i++) {
+            if (i == nums.length || nums[i] != nums[i - 1] + 1) {
+                if (start == nums[i - 1]) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + nums[i - 1]);
                 }
-                prev = nums[i+1];
+                if (i < nums.length) start = nums[i];
             }
         }
-        if (nums[nums.length-1] == prev){
-            answer.add(Integer.toString(nums[nums.length-1]));
-        }else{
-            answer.add(prev + "->"+nums[nums.length-1]);
-        }
-        return answer;
+        return result;
     }
 }
